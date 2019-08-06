3578be06ce4e1d24a09e945913511f33a8129a3d
Hamza Ait Oumghar <hamza.aitoumghar.3@gmail.com>
2019-08-06 10:15:09 +0100

    add Dashbord

---
+++ src/component/layout/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-primary mb-4">
+                <div className="container">
+                    <a className="navbar-brand" href="#">Project Management</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item ">
+                                <a className="nav-link" href="#">Dashboard</a>
+                            </li>
+
+                        </ul>
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item ">
+                                <a className="nav-link" href="#">Sign Up</a>
+                            </li>
+                            <li className="nav-item ">
+                                <a className="nav-link" href="#">Logout</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+export default Header;
